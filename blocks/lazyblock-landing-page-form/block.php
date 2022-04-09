92f41981c2474547b585ff8873339c0808db9f93
Daniil <boiko.81@list.ru>
2022-04-09 15:43:42 +0300

    connect contact-form-7

--- blocks/lazyblock-landing-page-form/block.php
+++ blocks/lazyblock-landing-page-form/block.php
@@ -3,6 +3,8 @@
         <h2 class="home-form__title" data-aos="fade-in">
             <?php echo $attributes['title']?>
         </h2>
-        <div class="home-form-inner" data-aos="fade-in"></div>
+        <div class="home-form-inner" data-aos="fade-in">
+            <?php echo do_shortcode('[contact-form-7 id="49" title="Home Contact Form"]')?>
+        </div>
     </div>
 </section>
