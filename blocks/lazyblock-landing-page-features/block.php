6ad98d5f0d7e7ac7fc5e717d7309584e236b4b70
Daniil <boiko.81@list.ru>
2022-04-09 15:21:00 +0300

    features block update

---
+++ blocks/lazyblock-landing-page-features/block.php
@@ -0,0 +1,26 @@
+<section class="home-features">
+    <div class="container">
+        <h2 class="home-features__title" data-aos="fade-in">Key features</h2>
+        <div class="home-features__list">
+
+        <?php 
+        $delay = 0;
+        foreach ($attributes['features'] as $feature): 
+            $delay = $delay + 100;
+        ?>
+            <div class="home-features__list-item" data-aos="fade-right" data-aos-delay="<?php echo $delay ?>">
+                <div class="home-features__list-item_image">
+                    <img src="<?php echo $feature['feature-icon']['url'] ?>" alt="<?php echo $feature['feature-icon']['alt'] ?>">
+                </div>
+                <h3 class="home-features__list-item_title">
+                    <?php echo $feature['feature-title'] ?>
+                </h3>
+                <p class="home-features__list-item_desc">
+                    <?php echo $feature['feature-description'] ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
+        
+        </div>
+    </div>
+</section>
--- functions.php
+++ functions.php
@@ -49,12 +49,12 @@ add_filter( 'wp_check_filetype_and_ext', 'fix_svg_mime_type', 10, 5 );
 
 function fix_svg_mime_type( $data, $file, $filename, $mimes, $real_mime = '' ){
 
-	if( version_compare( $GLOBALS['wp_version'], '5.1.0', '>=' ) )
+	if( version_compare( $GLOBALS['wp_version'], '5.1.0', '>=' ) ){
 		$dosvg = in_array( $real_mime, [ 'image/svg', 'image/svg+xml' ] );
-	else
+	} else {
 		$dosvg = ( '.svg' === strtolower( substr($filename, -4) ) );
-
-	if( $dosvg ){
+	}
+	if( $dosvg ) {
 
 		if( current_user_can('manage_options') ){
 
